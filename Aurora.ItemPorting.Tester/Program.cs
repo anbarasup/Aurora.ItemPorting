018e7fe01fdbf67c73be3e0608ff7899f06ebe02
dsekar <dhanasekar.natarajan@gmail.com>
2013-06-22 11:07:58 +0530

    Testing project changes

--- Aurora.ItemPorting.Tester/Program.cs
+++ Aurora.ItemPorting.Tester/Program.cs
@@ -11,7 +11,6 @@ namespace Aurora.ItemPorting.Tester
     {
         static void Main(string[] args)
         {
-
             //ows.AddFromTemplate(new Guid("{0DE95AE4-41AB-4D01-9EB0-67441B7C2450}"), new Guid("{76036F5E-CBCE-46D1-AF0A-4143F9B557AA}"), "TestSample3");
             //IEnumerable<Item> childern = ows.GetChildern(new Guid("{A0CCF70A-5F87-454B-8A1B-05EEA2DF5FD5}"));
             //IEnumerable<ItemField> itemfields = ows.GetItemFields(new Item() { Id = new Guid("{2B9A2A9B-8E69-43AB-BB6F-60AEC55CB364}") }, "en", 0, false);
