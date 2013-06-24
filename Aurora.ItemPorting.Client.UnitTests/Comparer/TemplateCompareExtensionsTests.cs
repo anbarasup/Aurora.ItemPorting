149320bcdee66a28bbc6fc6428ffc9a7466404b8
dsekar <dhanasekar.natarajan@gmail.com>
2013-06-24 15:36:10 +0530

    Unit Tests For DataService

--- Aurora.ItemPorting.Client.UnitTests/Comparer/TemplateCompareExtensionsTests.cs
+++ Aurora.ItemPorting.Client.UnitTests/Comparer/TemplateCompareExtensionsTests.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-namespace Aurora.ItemPorting.Client.UnitTests
+namespace Aurora.ItemPorting.Client.UnitTests.Comparer
 {
     [TestClass]
     public class TemplateCompareExtensionsTests
---
+++ Aurora.ItemPorting.Client.UnitTests/Data/OldWebServiceTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Aurora.ItemPorting.Client.Data;
+using Aurora.ItemPorting.Client.Data.Entities;
+
+namespace Aurora.ItemPorting.Client.UnitTests.Data
+{
+    [TestClass]
+    public class OldWebServiceTests
+    {
+        [TestMethod]
+        public void GetDatabases()
+        {
+            WebService ows = DataServiceFactory.Instance.Create("OldWebSevice"
+                                                                , new Connection(new Uri("http://Sitecore680/sitecore/shell/webservice/service.asmx")
+                                                                , new Credentials(@"sitecore\admin", "b"), "master"));
+            IEnumerable<Database> owsdata = ows.GetDatabases();
+        }
+    }
+}
